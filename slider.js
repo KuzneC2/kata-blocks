43096241d75dc7cc0f1b09eac66f85698666dca8
Dmitriy Kuznetsov <grebi.taban@mail.ru>
2024-06-25 09:13:09 +0300

    Слайдер блок  1

--- slider.js
+++ slider.js
@@ -1,21 +1,45 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
+    slidesPerView: 2,
+    spaceBetween: 190,
+
+    breakpoints: {
+        320: {
+
+            spaceBetween: 190,
+        },
+
+        420: {
+            spaceBetween: 120,
+        },
+    },
+
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
+        clickable: true,
     },
-  
-    // Navigation arrows
+
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
+
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  });
+});
+
+
+const slider = document.querySelector('.slider');
+const slideMore = document.querySelector('.slide-more');
+
+slideMore.addEventListener('click', function () {
+    slider.classList.toggle('open');
+    if (slideMore.textContent === 'Показать все') {
+        slideMore.textContent = 'Скрыть'
+    }
+    else {
+        slideMore.textContent = 'Показать все'
+    }
+});
