9488bdae823aedb0195948cc25ca151f075fe10e
Dmitriy Kuznetsov <grebi.taban@mail.ru>
2024-06-30 21:24:28 +0300

    Собрал блоки вместе

--- src/index.js
+++ src/index.js
@@ -1,2 +1,61 @@
 import './styles/index.css';
-console.log('Hello world!')
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination],
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 190,
+
+    breakpoints: {
+        320: {
+
+            spaceBetween: 190,
+        },
+
+        420: {
+            spaceBetween: 120,
+        },
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+
+});
+
+const slider = document.querySelectorAll('.slider');
+const slideMore = document.querySelectorAll('.slide-more');
+
+
+for (let i = 0; i < slideMore.length; i++) {
+    slideMore[i].addEventListener('click', function () {
+        slider[i].classList.toggle('open');
+        if (slideMore[i].textContent === 'Показать все') {
+            slideMore[i].textContent = 'Скрыть'
+        }
+        else {
+            slideMore[i].textContent = 'Показать все'
+        }
+
+
+    })
+}
+
+
+
+
+
+// slideMore.addEventListener('click', function () {
+//     slider.classList.toggle('open');
+//     if (slideMore.textContent === 'Показать все') {
+//         slideMore.textContent = 'Скрыть'
+//     }
+//     else {
+//         slideMore.textContent = 'Показать все'
+//     }
+// });
