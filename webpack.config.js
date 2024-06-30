8785ba96ec79e076035372ef11e1a1261bf4c7ae
Dmitriy Kuznetsov <grebi.taban@mail.ru>
2024-06-30 14:23:49 +0300

    Настройка вебпака

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|ico|mp4)$/i,
+                type: 'asset',
+            },
+
+
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/pages/index.html'
+        })
+    ]
+}
